2a6b027b735f1d6d562921aa187a3d6961179d15
Erich Hils <mrerichhils@gmail.com>
2022-01-14 15:38:34 -0600

    initial seed docs created, see funtions created

---
+++ src/scripts/main.js
@@ -0,0 +1,9 @@
+import { createPlan } from "./plan.js"
+console.log("Welcome to the main module")
+const yearlyPlan = createPlan()
+console.log(yearlyPlan)
+import { createAsparagus } from "./seeds/asparagus.js"
+
+const asparagusSeed = createAsparagus()
+console.log(asparagusSeed)
+
---
+++ src/scripts/seeds/asparagus.js
@@ -0,0 +1,8 @@
+export const createAsparagus = (object) => {
+    const asparagusPlant = {
+        type: "asparagus",
+        height: 24,
+        output: 4
+    }
+    return asparagusPlant
+}
