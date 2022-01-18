836087795b535fb1cc952aca4e45f35af762ad9b
Erich Hils <mrerichhils@gmail.com>
2022-01-18 15:35:42 -0600

    harvester.jss added

---
+++ src/scripts/harvester.js
@@ -0,0 +1,10 @@
+// define a harvestPlants fucntion
+// accept plants array as input
+// return an array of seed objects
+// iterate array of growing plants
+// on each plant get the value of the output property
+
+const harvestPlants = (plantArr) => {
+    let seedObj = 
+    return seedObj
+}
